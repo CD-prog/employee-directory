bc3995f1b038068275fb0e8a9c9234fcaf49e0c4
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-09-21 22:22:32 -0500

    getting employees

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+  const [search, setSearch] = useState('')
+  const [employees, setEmployees] = useState([])
+  const employeesFull = React.useRef(null)
+
+  useEffect(() => {
+    getEmployees()
+  }, [])
+
+  useEffect(() => {
+    if (employeesFull.current) {
+      console.log('Filtering...', employeesFull);
+      setEmployees(employeesFull.current.filter(employee => {
+        const name = `${employee.name.first} ${employee.name.last}`.toLowerCase();
+        return name.indexOf(search) !== -1;
+      }))
+    }
+  }, [search])
+
+  const getEmployees = async () => {
+    const res = await axios.get(`https://randomuser.me/api/?results=200&nat=us`)
+    console.log(res.data.results)
+    employeesFull.current = res.data.results;
+    setEmployees(employeesFull.current)
+  }
+  const isLoading = employeesFull.current === null;
+  const isEmpty = employees.length === 0;
+  console.log(employeesFull)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? <div>Loading...</div> :
+        (
+          <>
+            <header>
+              <h3>
+                Employee Directory
+              </h3>
+            </header>
+            
+            <form>
+              <input onChange={(evt) => setSearch(evt.currentTarget.value)} placeholder="Search Employees" type="text" name="name" value={search} />
+            </form>
+            
+            {isEmpty ? <div>No Results...</div> : null}
+
+            <div>
+            {employees.map((employee) => <p key={employee.login.uuid}>{employee.name.first} {employee.name.last}</p>)}
+            </div>
+          </>)
+      }
     </div>
   );
 }
